2c3a7090e4d959851166b65da78b5d1c425ce58d
Sara Spink and Tory Bryant <tory@>
2017-06-05 18:08:02 -0700

    fix a problem and get the js working

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var voterInput = $("input#voter").val();
-    var ageInput = $("input#age").val();
+    var ageInput = parseInt($("input#age").val());
     var mascotInput= $("input#mascot").val();
+
+    if (ageInput > 17) {
+      $("#over-age").show();
+    } else {
+      $("#thankYou").show();
+    }
 
 
     $(".voter").text(voterInput);
@@ -10,8 +16,8 @@ $(document).ready(function() {
     $(".mascot").text(mascotInput);
 
 
-    $("#thankYou").show();
-    $("#over-age").show();
+    // $("#thankYou").show();
+    // $("#over-age").show();
     event.preventDefault();
-  });
+    });
 });
